dde70b7d81209eb56a3dced3d95f28d40f6e5603
Josshy <josshy92@hotmail.com>
2021-10-25 18:34:00 -0500

    changed code to href linds to fix 404 page

--- highscores.js
+++ highscores.js
@@ -1,10 +1,6 @@
 var highScoresList = document.querySelector("#highScoresList")
 var highScores = JSON.parse(localStorage.getItem("highScores")) || []
 
-// function init () {
-//     var storedScores = 
-// }
-// localStorage.setItem("highScores", highScores);
 
 highScoresList.innerHTML =
 highScores.map(score => {
