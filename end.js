99cbbf49e876001bd238edb9ab6825f30228173c
Josshy <josshy92@hotmail.com>
2021-10-25 18:36:38 -0500

    added href links to end page to go back to homescreen

--- end.js
+++ end.js
@@ -30,5 +30,5 @@ saveHighScore = e => {
     highScores.splice(5)
 
     localStorage.setItem("highScores", JSON.stringify(highScores))
-    window.location.href="/"
+    window.location.href="https://josshy92.github.io/homework-04/"
 }
